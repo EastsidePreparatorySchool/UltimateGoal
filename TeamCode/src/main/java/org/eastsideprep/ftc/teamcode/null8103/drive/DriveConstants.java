50dc758b338b8cbedd4ec8c9ab8fe354cb66ed95
TEspelien <tespelien@eastsideprep.org>
2020-11-30 21:26:07 -0800

    most tuning done, passed spline test

--- TeamCode/src/main/java/org/eastsideprep/ftc/teamcode/null8103/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/eastsideprep/ftc/teamcode/null8103/drive/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 50 * mm; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 1; // in
+    public static double TRACK_WIDTH = 13.3; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
