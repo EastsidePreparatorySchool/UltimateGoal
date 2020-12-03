893ec11fa68c096ad5cf8e128edce3f262cc4400
TEspelien <tespelien@eastsideprep.org>
2020-12-02 22:48:36 -0800

    added template teleop

--- TeamCode/src/main/java/org/eastsideprep/ftc/teamcode/null8103/Teleop_template_copy_file_and_rename.java
+++ TeamCode/src/main/java/org/eastsideprep/ftc/teamcode/null8103/Teleop_template_copy_file_and_rename.java
@@ -29,17 +29,14 @@
 
 package org.eastsideprep.ftc.teamcode.null8103;
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@Disabled
+@TeleOp(name = "Teleop Template")
 
-@TeleOp(name = "Teleop A")
-
-//simple drive teleop
-public class Teleop_A extends LinearOpMode {
+public class Teleop_template_copy_file_and_rename extends LinearOpMode {
 
     /* Declare OpMode members. */
     RobotHardware robot = new RobotHardware();
@@ -58,44 +55,7 @@ public class Teleop_A extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        MecanumDrive mecanumDrive = new MecanumDrive(robot.leftFront, robot.rightFront, robot.leftBack, robot.rightBack);
-        GamepadEx gamepad = new GamepadEx(gamepad1);
-
-
-        //double speedControl;
-
-        //both of these are tuned manually to driver preference
-        //used in the ether function to map gamepad -> drivetrain controls
-        double driveGain = 0.6;
-        double turnGain = 0.3;
-
-        double ySpeed, xSpeed, turnSpeed, gyroAngle;
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-//            if (gamepad.getButton(GamepadKeys.Button.LEFT_BUMPER)) {
-//                speedControl = 0.4;
-//            } else {
-//                speedControl = 1;
-//            }
-            xSpeed = ether(gamepad.getLeftX(), driveGain);
-            ySpeed = ether(gamepad.getLeftY(), driveGain);
-            turnSpeed = ether(gamepad.getRightX(), turnGain);
-            //gyroAngle = Math.toDegrees(robot.revIMU.getAbsoluteHeading() * Math.PI + Math.PI);//imu data is a double from -1 to 1, convert to 0 to 2pi
-            mecanumDrive.driveRobotCentric(xSpeed, ySpeed, turnSpeed, true);
-            //mecanumDrive.driveFieldCentric(xSpeed, ySpeed, turnSpeed, gyroAngle, true);//squaring inputs is more precise
-            //telemetry.addData("imu data", gyroAngle);
-            telemetry.update();
-        }
     }
 
-    //see graph at https://www.desmos.com/calculator/dx7yql2ekh
-    public static double ether(double x, double p) {
-        double min = 0.2; //this means that very small joystick movements give enough power to overcome friction
-        double max = 1; //max power is 1
-        if (x < 0) {
-            min = -min;
-        }
-        return min + (1 - min) * (p * Math.pow(x, 3) + (1 - p) * x);
-    }
 }
 
